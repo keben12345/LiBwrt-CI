15f15311ff6687a3fba91c1b83302f32bf155026
keben12345 <79047578+keben12345@users.noreply.github.com>
2024-08-26 18:53:25 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -43,7 +43,7 @@ if [[ $WRT_URL == *"lede"* ]]; then
 else
 	echo "CONFIG_PACKAGE_luci=y" >> ./.config
 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
-        echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config	
+        #echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config	
  #echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 fi
 
